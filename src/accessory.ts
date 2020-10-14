b6c9deb97131fd7ab5bbb670a020d9cdf36331ba
Jasper Patterson <jasper@jasperpatterson.me>
2020-10-13 21:01:06 -0600

    remove custom state management

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,7 +1,5 @@
 import {
-	CharacteristicChange,
 	CharacteristicEventTypes,
-	CharacteristicGetCallback,
 	CharacteristicSetCallback,
 	CharacteristicValue,
 	PlatformAccessory,
@@ -13,8 +11,6 @@ import { controlLock } from "./abode/api";
 
 export class AbodeLockAccessory {
 	public service: Service;
-
-	private LockCurrentState: CharacteristicValue;
 
 	constructor(private readonly platform: AbodeLocksPlatform, private readonly accessory: PlatformAccessory) {
 		this.accessory
@@ -31,20 +27,8 @@ export class AbodeLockAccessory {
 		this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
 
 		this.service
-			.getCharacteristic(this.platform.Characteristic.LockCurrentState)
-			.on(CharacteristicEventTypes.GET, this.getLockState.bind(this))
-			.on(CharacteristicEventTypes.CHANGE, this.lockStateChanged.bind(this));
-
-		this.service
 			.getCharacteristic(this.platform.Characteristic.LockTargetState)
-			.on(CharacteristicEventTypes.GET, this.getLockState.bind(this))
 			.on(CharacteristicEventTypes.SET, this.setLockState.bind(this));
-	}
-
-	getLockState(callback: CharacteristicGetCallback) {
-		this.platform.log.debug("getLockState", this.accessory.context.device.id);
-
-		callback(null, this.LockCurrentState);
 	}
 
 	async setLockState(value: CharacteristicValue, callback: CharacteristicSetCallback) {
@@ -52,26 +36,11 @@ export class AbodeLockAccessory {
 
 		try {
 			const status = this.platform.convertLockTargetStateToAbodeLockStatusInt(value);
-
 			await controlLock(this.accessory.context.device.id, status);
-
 			callback();
-
-			setTimeout(() => {
-				if (status !== this.LockCurrentState) {
-					this.platform.log.debug("LockCurrentState does not match, force updating");
-					this.platform.updateStatus();
-				}
-			}, 15000);
 		} catch (error) {
 			this.platform.log.error("setLockState failed", error.message);
 			callback(error);
 		}
-	}
-
-	lockStateChanged(change: CharacteristicChange) {
-		this.platform.log.debug("lockStateChanged", this.accessory.context.device.id);
-
-		this.LockCurrentState = change.newValue;
 	}
 }
